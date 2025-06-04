86b1938411c266db8b57e6ab2c7436cb120d7110
Thiennh <thienngohoang99@gmail.com>
2025-06-05 00:07:14 +0700

    Màn hình login

---
+++ app/src/main/java/com/example/baseapp/view/login/LoginView.java
@@ -0,0 +1,40 @@
+package com.example.baseapp.view.login;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.example.baseapp.view.base.BaseView;
+
+public class LoginView extends BaseView {
+
+
+    public LoginView(@NonNull Context context) {
+        super(context);
+    }
+
+    @Override
+    public int getViewLayout() {
+        return 0;
+    }
+
+    @Override
+    public void initElementLayouts() {
+
+    }
+
+    @Override
+    public void setValue() {
+
+    }
+
+    @Override
+    public void setEvent() {
+
+    }
+
+    @Override
+    public void configHeaderBar() {
+
+    }
+}
